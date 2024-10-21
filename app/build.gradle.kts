a34c60439a1d84ae56356101e2b5021b5b774c87
Ali Sadeghi <alisadeghi.dev@gmail.com>
2024-10-21 18:55:44 +0330

    Remove test files and dependencies.
    Will add manually later.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,12 +52,6 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
