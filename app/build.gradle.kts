198a6bafd9a3ac15643b6bccc8a8a1a69255e645
Ali Sadeghi <alisadeghi.dev@gmail.com>
2024-10-21 11:53:25 +0330

    Update targetSdk to 35.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.sadeghi.animation"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.sadeghi.animation"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
