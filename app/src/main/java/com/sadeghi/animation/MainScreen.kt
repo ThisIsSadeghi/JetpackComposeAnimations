d7a645a2c914c6509fc6ae5a52b2055a0ba20cc6
Ali Sadeghi <alisadeghi.dev@gmail.com>
2024-10-23 17:52:42 +0330

    Modify some UI elements.

--- app/src/main/java/com/sadeghi/animation/MainScreen.kt
+++ app/src/main/java/com/sadeghi/animation/MainScreen.kt
@@ -1,15 +1,22 @@
 package com.sadeghi.animation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 /**
  * Created by Ali Sadeghi
@@ -21,14 +28,30 @@ fun MainScreen(
     modifier: Modifier = Modifier,
     onVisibleInvisibleClick: () -> Unit = {},
 ) {
-    Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.spacedBy(16.dp)) {
-        Button(modifier = Modifier.fillMaxWidth(), onClick = onVisibleInvisibleClick) {
-            Text(text = "Visible/Invisible")
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(painter = painterResource(id = R.drawable.compose), contentDescription = "Compose logo")
+
+        Text(text = "Jetpack Compose Animations", fontSize = 24.sp)
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            item {
+                Button(modifier = Modifier.fillMaxWidth(), onClick = onVisibleInvisibleClick) {
+                    Text(modifier = Modifier.align(Alignment.CenterVertically), text = "Visibility")
+                }
+            }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun MainScreenPreview() {
     MainScreen()
